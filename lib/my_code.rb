e3db08238f674bdb2128d6ed81ba7c3ef544f31f
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-03 18:22:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -59,3 +59,12 @@ def reduce_to_all_true(source_array)
   end
   return true
 end
+
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return true if source_array[i] === true
+    i +=1
+  end
+  return false
+end
