b220b2bb27d21451f7698e1add219cc53f62f9b4
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-03 18:18:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,3 +20,13 @@ while i < source_array.length do
 end
   return new_array
 end
+
+def map_to_double(source_array)
+new_array = []
+i = 0
+while i < source_array.length do
+  new_array.push(source_array[i] *2)
+  i +=1
+end
+return new_array
+end
