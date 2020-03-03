e71229c931af396d426bcacc6bcca2641451cfcd
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-03 18:15:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,10 @@
 # My Code here....
 
-map_to_negativize = [1, 2, 3, -9].map
+new_array = []
+i = 0
+while i < source_array.length do
+  new_array.push(source_array[i] * -1)
+  i +=1
+end
+return new_array
+end
